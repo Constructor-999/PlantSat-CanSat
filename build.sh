c3bf31ea7449a7e585601c7cef4d0012da4637c1
Julien <j_durrer@hotmail.com>
2023-04-27 12:50:36 +0200

    fix sudo echo error

--- build.sh
+++ build.sh
@@ -9,8 +9,9 @@ pip install -r requirements.txt
 while true; do
     read -p "Do you wish to create a service ?" yn
     case $yn in
-        [Yy]* ) sudo echo "[Unit]\nDescription=PlantSat service\nAfter=multi-user.target\n\r[Service]\nType=simple\nRestart=always\nExecStart=/usr/bin/python3 $(pwd)/main.py\n\r[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/plantsat.service; sudo systemctl start plantsat.service; echo "sevice is running"; break;;
+        [Yy]* ) echo "[Unit]\nDescription=PlantSat service\nAfter=multi-user.target\n\r[Service]\nType=simple\nRestart=always\nExecStart=/usr/bin/python3 $(pwd)/main.py\n\r[Install]\nWantedBy=multi-user.target" > $(pwd)/plantsat.service; sudo mv $(pwd)/plantsat.service /etc/systemd/system;sudo systemctl start plantsat.service; echo "sevice is starting"; echo "cleaning up"; sudo rm $(pwd)/requirements.txt;break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+rm $(pwd)/build.sh
